0f3d3962487c7963ea6557e80e8f7824eea03959
Christoph Kick <kick.christoph@gmail.com>
2023-11-02 16:33:24 +0100

    Extract List and Search components

--- src/App.tsx
+++ src/App.tsx
@@ -41,24 +41,38 @@ function App() {
       <h1>
         {welcomeStrings.greeting} {welcomeStrings.title}
       </h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}:</a>
-            </span>
-            <span>, {item.author}</span>
-            <span>, {item.num_comments}</span>
-            <span>, {item.points}</span>
-          </li>
-        ))}
-      </ul>
+      <List />
     </div>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}:</a>
+          </span>
+          <span>, {item.author}</span>
+          <span>, {item.num_comments}</span>
+          <span>, {item.points}</span>
+        </li>
+      ))}
+    </ul>
   );
 }
 
